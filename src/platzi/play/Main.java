742b3c808e374adbc0d2d40f2bc30a7378119974
Carlos Augusto <22408566+augusto2501@users.noreply.github.com>
2025-10-05 15:03:13 -0500

    Initial commit

---
+++ src/platzi/play/Main.java
@@ -0,0 +1,18 @@
+package platzi.play;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Cual es tu nombre");
+        String nombre = scanner.nextLine();
+        System.out.println("Hola " + nombre + ", esto es una prueba");
+
+        System.out.println("Cuantos años tienes");
+        int edad = scanner.nextInt();
+        System.out.println();
+    }
+}
